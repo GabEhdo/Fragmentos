22066b7fb7384071c671a49252dce51b847854be
Carmen Gabriela Escobedo Hernandez <carmengabriela.escobedohernandez@nttdata.com>
2022-06-02 09:44:23 -0500

    subir app

---
+++ app/build/generated/ap_generated_sources/debug/out/com/example/fragments/BR.java
@@ -0,0 +1,5 @@
+package com.example.fragments;
+
+public class BR {
+  public static final int _all = 0;
+}
---
+++ app/build/generated/data_binding_trigger/debug/com/example/fragments/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.example.fragments;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
